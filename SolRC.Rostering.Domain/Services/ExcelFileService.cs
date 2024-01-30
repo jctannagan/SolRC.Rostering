9ea81e8eafda814c8e55f1270d89af3cf22a11f4
Kath <kathreen.camon@gmail.com>
2024-01-30 19:07:27 +0800

    List To Exccel service

--- SolRC.Rostering.Domain/Services/ExcelFileService.cs
+++ SolRC.Rostering.Domain/Services/ExcelFileService.cs
@@ -8,26 +8,67 @@ public class ExcelFileService : IExcelFileService
 {
     public void ListToExcel(List<TableAssignment> tableAssignments)
     {
-        // Create a new Excel package
         using (var package = new ExcelPackage())
         {
-            // Add a new worksheet to the empty workbook
             var worksheet = package.Workbook.Worksheets.Add("Sheet1");
 
-            // Load data into the worksheet, starting from cell A1
-            for (int i = 0; i < tableAssignments.Count; i++)
+            var filth = tableAssignments.Select(t => t)
+                .Distinct().OrderBy(t => (t.ScheduleDate, t.Employee.FirstName, t.Employee.LastName)).ToList();
+
+            for (int x = 0; x < filth.Count; x++)
             {
-                worksheet.Cells[i + 1, 1].Value = tableAssignments[i];
+                // date will be the headers
+                worksheet.Cells[x + 1, 1].Value = filth[x].ScheduleDate.ToString();
+
+                var employeeName = $"{filth[x].Employee.FirstName} {filth[x].Employee.LastName}";
+                worksheet.Cells[x + 1, 2].Value = employeeName;
+
+                var tableShift = $"{filth[x].Table.Name} {filth[x].Hours.ShiftClass}";
+                worksheet.Cells[x + 1, 3].Value = tableShift;
+
             }
 
-            // Define the file name
             var fileName = "ExportedData.xlsx";
             var fileInfo = new FileInfo(fileName);
 
-            // Save the Excel package
             package.SaveAs(fileInfo);
 
             Console.WriteLine($"File saved to {fileInfo.FullName}");
         }
     }
+
+    //public void ListToExcel(List<TableAssignment> tableAssignments)
+    //{
+    //    using (var package = new ExcelPackage())
+    //    {
+    //        var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+    //        var distinctDates = tableAssignments.Select(t => t.ScheduleDate)
+    //            .Distinct().OrderBy(t => t.Date).ToList();
+    //        for (int x = 0; x < distinctDates.Count; x++)
+    //        {
+    //            // date will be the headers
+    //            worksheet.Cells[1, x + 2].Value = distinctDates[x].ToString();
+
+    //            var dateFilteredTableAssignments = tableAssignments
+    //                .Where(t => t.ScheduleDate == distinctDates[x]).ToList();
+
+    //            for (int y = 0; y < dateFilteredTableAssignments.Count; y++)
+    //            {
+    //                var employeeName = $"{dateFilteredTableAssignments[y].Employee.FirstName} {dateFilteredTableAssignments[y].Employee.LastName}";
+    //                worksheet.Cells[y + 2, 1].Value = employeeName;
+
+    //                var tableShift = $"{dateFilteredTableAssignments[y].Table.Name} {dateFilteredTableAssignments[y].Hours.ShiftClass}";
+    //                worksheet.Cells[y + 2, x + 2].Value = tableShift;
+    //            }
+    //        }
+
+    //        var fileName = "ExportedData.xlsx";
+    //        var fileInfo = new FileInfo(fileName);
+
+    //        package.SaveAs(fileInfo);
+
+    //        Console.WriteLine($"File saved to {fileInfo.FullName}");
+    //    }
+    //}
 }
