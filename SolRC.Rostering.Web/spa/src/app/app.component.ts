6fdbdf0c8f71f61618871a45d940de7a1a39f9df
Jeff Tannagan <Jeff.Tannagan@mgt.bdo.global>
2024-01-31 17:23:32 +0800

    added boilerplate angular 17

--- SolRC.Rostering.Web/spa/src/app/app.component.ts
+++ SolRC.Rostering.Web/spa/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NavMenuComponent} from "./nav-menu/nav-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
