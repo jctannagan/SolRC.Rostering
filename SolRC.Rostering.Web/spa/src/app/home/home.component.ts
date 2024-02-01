e8c679189bf4fafc1b3dc41ecc170af925d4f14e
Kath <kathreen.camon@gmail.com>
2024-02-01 18:44:41 +0800

    button

--- SolRC.Rostering.Web/spa/src/app/home/home.component.ts
+++ SolRC.Rostering.Web/spa/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScheduleApiService } from '../services/schedule-api-service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private scheduleApiService: ScheduleApiService) { }
 
+  public onGenerateSchedule(): void {
+    //this.scheduleApiService.generateSchedule().subscribe({
+    //  next: () => console.log("next"),
+    //  error: (e) => console.error(e),
+    //  complete: () => console.log("complete"),
+    //});
+
+    this.scheduleApiService.generateSchedule().subscribe(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const anchor = document.createElement('a');
+      anchor.download = 'yourfile.xlsx';
+      anchor.href = url;
+      anchor.click();
+      window.URL.revokeObjectURL(url);
+    });
+  }
 }
