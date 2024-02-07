ece0e105e61810f1e5adedd3406171daf998b79c
Jeff Tannagan <Jeff.Tannagan@mgt.bdo.global>
2024-02-07 16:39:20 +0800

    added border styling

--- SolRC.Rostering.Domain/Services/ExcelFileService.cs
+++ SolRC.Rostering.Domain/Services/ExcelFileService.cs
@@ -1,5 +1,7 @@
-﻿using SolRC.Rostering.Domain.Models;
+﻿using System.Drawing;
+using SolRC.Rostering.Domain.Models;
 using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using SolRC.Rostering.Domain.Contracts.Services;
 
 namespace SolRC.Rostering.Domain.Services;
@@ -48,26 +50,63 @@ public class ExcelFileService : IExcelFileService
     {
         using (var package = new ExcelPackage())
         {
+            var dates = tableAssignments.Select(p => p.ScheduleDate).Distinct();
+            var minDate = dates.Min();
+            var maxDate = dates.Max();
+            
+            Color headerBgColor = ColorTranslator.FromHtml("#FFC000");
+            
             var worksheet = package.Workbook.Worksheets.Add("Sheet1");
-            worksheet.Cells[1, 1].Value = "EmpNumber";
-            worksheet.Cells[1, 1].Style.Font.Bold = true;
-
-            worksheet.Cells[1, 2].Value = "Name";
-            worksheet.Cells[1, 2].Style.Font.Bold = true;
+            worksheet.Cells[1, 3, 1, 4].Merge = true;
+            string dateHeader = string.Empty;
+            if (minDate.Month == maxDate.Month)
+                dateHeader = $"{minDate.Day}-{maxDate.Day} {maxDate.ToString("MMMM, yyy")}";
+            else
+                dateHeader = $"{minDate.ToString("d MMM")}-{maxDate.ToString("d MMM, yyy")}";
+            
+            worksheet.Cells[1, 3].Value = dateHeader;
+            worksheet.Cells[1, 3].Style.Font.Bold = true;
+            
+            worksheet.Cells[2, 1].Value = "EmpNumber";
+            worksheet.Cells[2, 1].Style.Font.Bold = true;
+            worksheet.Cells[2, 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            worksheet.Cells[2, 1].Style.Fill.BackgroundColor.SetColor(headerBgColor);
+            worksheet.Cells[2, 1].Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+            worksheet.Cells[2, 1].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+            worksheet.Cells[2, 1].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+            worksheet.Cells[2, 1].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+            
+            worksheet.Cells[2, 2].Value = "Name";
+            worksheet.Cells[2, 2].Style.Font.Bold = true;
+            worksheet.Cells[2, 2].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            worksheet.Cells[2, 2].Style.Fill.BackgroundColor.SetColor(headerBgColor);
+            worksheet.Cells[2, 2].Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+            worksheet.Cells[2, 2].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+            worksheet.Cells[2, 2].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+            worksheet.Cells[2, 2].Style.Border.Right.Style = ExcelBorderStyle.Thin;
 
             var distinctDates = tableAssignments.Select(t => t.ScheduleDate)
                 .Distinct().OrderBy(t => t.Date).ToList();
             var column = 3;
+            
             for (int x = 0; x < distinctDates.Count; x++)
             {
                 // date will be the headers
-                worksheet.Cells[1, column, 1, column + 1].Merge = true;
-                worksheet.Cells[1, column].Value = distinctDates[x].ToShortDateString();
-                worksheet.Cells[1, column].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
-                worksheet.Cells[1, column].Style.Font.Bold = true;
+                worksheet.Cells[2, column, 2, column + 1].Merge = true;
+                worksheet.Cells[2, column].Value = distinctDates[x].ToString("ddd d");
+                worksheet.Cells[2, column].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                worksheet.Cells[2, column].Style.Font.Bold = true;
+                worksheet.Cells[2, column].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                worksheet.Cells[2, column].Style.Fill.BackgroundColor.SetColor(headerBgColor);
+                worksheet.Cells[2, column].Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+                worksheet.Cells[2, column + 1].Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+                worksheet.Cells[2, column].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[2, column + 1].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[2, column].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[2, column + 1].Style.Border.Right.Style = ExcelBorderStyle.Thin;
 
                 var allEmployees = _employeeService.GetAll().OrderBy(e => e.Number).ToList();
-                var row = 2;
+                var row = 3;
                 for (int y = 0; y < allEmployees.Count; y++)
                 {
                     worksheet.Cells[row, 1].Value = allEmployees[y].Number;
